085f4a50655f66f02adb777ef9d6c358c5f520b3
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-05 09:24:28 -0500

    understanding variable decleration and initialization

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,2 +1,22 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO!");
+Console.WriteLine("[R]emove a TODO!");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+userInput = "B";
+Console.WriteLine(userInput);
+
+int number = 2;
+Console.WriteLine(number);
+number = 3;
+Console.WriteLine(number);
+
+int a = 5, b = 2;
+string pie = "pie", taco = "taco";
+Console.WriteLine(a);
+Console.WriteLine(pie);
+
 Console.ReadKey();
--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bd939dca82484bf1c46f650aa3b05371ff31c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30857f0f503177eba7b81d442decf3693e5e69b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
