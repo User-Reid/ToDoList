42530a2c2e2643a5947c5c510699535d16ed6037
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-05 13:05:13 -0500

    understanding the $$ || operators :)

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -6,7 +6,10 @@ Console.WriteLine("[E]xit");
 
 var userInput = Console.ReadLine();
 
-bool taco = userInput == "ABC";
+var number = 10;
+var isLarger = number > 4 && number < 9;
+var isLarger2 = number == 2 || number > 6 || number < 200;
+
 
 
 // bool someBool = true;
