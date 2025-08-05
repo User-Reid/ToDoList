f1f8894209321340bd675ddaa82e0e37944ee4ff
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-05 10:17:42 -0500

    understanding operators :)

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,22 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO!");
-Console.WriteLine("[R]emove a TODO!");
-Console.WriteLine("[E]xit");
-
-string userInput = "A";
-Console.WriteLine(userInput);
-userInput = "B";
-Console.WriteLine(userInput);
-
-int number = 2;
-Console.WriteLine(number);
-number = 3;
-Console.WriteLine(number);
-
-int a = 5, b = 2;
-string banana = "pie", taco = "taco";
+﻿int a = 10;
+int b = 5;
+a++;
+b--;
 Console.WriteLine(a);
-Console.WriteLine(banana);
+Console.WriteLine(b);
+
+
+Console.WriteLine("Addition:" + (a + b));
+Console.WriteLine("Subtraction:" + (a - b));
+Console.WriteLine("Multiplication:" + a * b);
+Console.WriteLine("Division:" + a / b);
+Console.WriteLine("Reid" + " " + "Herrera");
+string word = "abc";
+Console.WriteLine(word + a);
 
 Console.ReadKey();
+
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TODOs");
+// Console.WriteLine("[A]dd a TODO!");
+// Console.WriteLine("[R]emove a TODO!");
+// Console.WriteLine("[E]xit");
+
+// string userInput = "A";
+// Console.WriteLine(userInput);
+// userInput = "B";
+// Console.WriteLine(userInput);
+
+// int number = 2;
+// Console.WriteLine(number);
+// number = 3;
+// Console.WriteLine(number);
+
+// int a = 5, b = 2;
+// string banana = "pie", taco = "taco";
+// Console.WriteLine(a);
+// Console.WriteLine(banana);
+
+// Console.ReadKey();
--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30857f0f503177eba7b81d442decf3693e5e69b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2f746a12f3e9f8875d09f35cc00e7cf126f954")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
