b3bd939dca82484bf1c46f650aa3b05371ff31c5
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-05 08:52:08 -0500

    understanding text files and executable programs.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLinee("Hello, World!");
+Console.ReadKey();
--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d54f2ced5177b421d7fc0b397495f6af07fa4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
