71f0e85590f1b52490399b78f388a38c6a9bb021
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-06 16:52:09 -0500

    understanding the while loop syntax in c#

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,38 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO!");
-Console.WriteLine("[R]emove a TODO!");
-Console.WriteLine("[E]xit");
+﻿using System.Globalization;
 
-var userInput = Console.ReadLine();
+int taco = 0;
 
-switch (userInput)
+while (taco < 10)
 {
-  case "S":
-  case "s":
-    PrintSelectedOption("See all TODO's");
-    break;
-  case "A":
-  case "a":
-    PrintSelectedOption("Add a TODO");
-    break;
-  case "R":
-  case "r":
-    PrintSelectedOption("Remove a TODO");
-    break;
-  case "E":
-  case "e":
-    PrintSelectedOption("Exit");
-    break;
-  default:
-    Console.WriteLine("Dog you messed up hard af fam😩");
-    break;
+  taco = taco + 1;
+  System.Console.WriteLine($"Number is: {taco}");
 }
 
-void PrintSelectedOption(string selectedOption)
-{
-  System.Console.WriteLine("Selected option: " + selectedOption);
-}
+System.Console.WriteLine("The loop is now finshed 😀");
+
+
+
+
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("[S]ee all TODOs");
+// Console.WriteLine("[A]dd a TODO!");
+// Console.WriteLine("[R]emove a TODO!");
+// Console.WriteLine("[E]xit");
+
+// var userInput = Console.ReadLine();
+
+// switch (userInput)
+// {
+//   case "S":
+//   case "s":
+//     PrintSelectedOption("See all TODO's");
+//     break;
+//   case "A":
+//   case "a":
+//     PrintSelectedOption("Add a TODO");
+//     break;
+//   case "R":
+//   case "r":
+//     PrintSelectedOption("Remove a TODO");
+//     break;
+//   case "E":
+//   case "e":
+//     PrintSelectedOption("Exit");
+//     break;
+//   default:
+//     Console.WriteLine("Dog you messed up hard af fam😩");
+//     break;
+// }
+
+// void PrintSelectedOption(string selectedOption)
+// {
+//   System.Console.WriteLine("Selected option: " + selectedOption);
+// }
 
 // if (userInput == "S")
 // {
