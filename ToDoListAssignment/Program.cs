d475fa35f8ac1a3b2d1a276751b7cadc4b61fc71
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-10 18:01:53 -0500

    double down completion

--- ToDoListAssignment/Program.cs
+++ ToDoListAssignment/Program.cs
@@ -1,114 +1,103 @@
-﻿System.Console.WriteLine("Hello!");
-
-bool shallExit = false;
+﻿List<string> toDoList = new List<string> { };
+bool hasExited = false;
 string userInput;
-string[] possibleOptions = new string[] { "a", "A", "s", "S", "E", "e", "r", "R" };
-List<string> toDoList = new List<string> {};
 
-while (!shallExit)
-{
-  
+
+System.Console.WriteLine("Howdy, welcome to the TODO list maker!");
+
 do
 {
-  System.Console.WriteLine("What do you want to do?");
+  System.Console.WriteLine("What would you like to do!");
   System.Console.WriteLine("[S]ee all TODO's");
   System.Console.WriteLine("[A]dd a TODO");
   System.Console.WriteLine("[R]emove a TODO");
   System.Console.WriteLine("[E]xit");
   userInput = Console.ReadLine();
-} while (!possibleOptions.Contains(userInput));
 
-switch (userInput)
-{
-  case "E":
-  case "e":
+  switch (userInput)
+  {
+    case "s":
+    case "S":
+      SeeAllTodo();
+      break;
+    case "a":
+    case "A":
+      AddTodo();
+      break;
+    case "R":
+    case "r":
+      RemoveTodo();
+      break;
+    case "e":
+    case "E":
       Exit();
-    break;
-  case "S":
-  case "s":
-      SeeAllTodos();
-    break;
-  case "A":
-  case "a":
-      AddToDo();
-    break;
-  case "R":
-  case "r":
-      RemoveToDo();
-    break;
-  default:
-    System.Console.WriteLine("oops");
-    break;
-  };
-}
+      break;
+    default:
+      System.Console.WriteLine("Please enter a valid option idiot.");
+      break;
+  }
+} while (!hasExited);
 
-
-void SeeAllTodos()
+void SeeAllTodo()
 {
   if (toDoList.Count == 0)
   {
-    System.Console.WriteLine("You aint got shit to do cuzin");
+    System.Console.WriteLine("You have no TODO list items jackass.");
   }
   else
   {
-  System.Console.WriteLine("See all TODO's");
-  for (int i = 0; i < toDoList.Count; i++)
-  {
-    System.Console.WriteLine($"{i + 1}: {toDoList[i]}");
-  }
+    System.Console.WriteLine("Here is your currenct TODO list!");
+    for (int i = 0; i < toDoList.Count; i++)
+    {
+      System.Console.WriteLine($"{i + 1}: {toDoList[i]}");
+    }
   }
 }
 
-List<string> AddToDo()
+void RemoveTodo()
 {
-  string addedTask;
-  System.Console.WriteLine("Add TODO");
+  string userIndex;
+  int index;
 
   do
   {
-    System.Console.WriteLine("What task would you like to add big dog?");
-    addedTask = Console.ReadLine();
-  } while (addedTask == "" || toDoList.Contains(addedTask));
+    System.Console.WriteLine("Which TODO would you like to remove?");
+    SeeAllTodo();
+    userIndex = Console.ReadLine();
+    index = int.Parse(userIndex);
+  } while (index <= 0 || index > toDoList.Count);
 
-  toDoList.Add(addedTask);
-  System.Console.WriteLine("Task successfully added big dog!🐶");
-  return toDoList;
+  toDoList.RemoveAt(index - 1);
+  System.Console.WriteLine("Your list has been successfully updated!");
 }
 
-List<string> RemoveToDo()
+void Exit()
 {
-  int indexInput;
-  string input;
-  if (toDoList.Count == 0)
+  System.Console.WriteLine("See ya later alligator!🐊");
+  hasExited = true;
+}
+
+void AddTodo()
+{
+  string userInput;
+
+  do
   {
-    System.Console.WriteLine("Try adding some task's jackass.");
-    return toDoList;
-  }
-  else
-  {
-    System.Console.WriteLine("Remove TODO");
-    do
+    System.Console.WriteLine("What TODO task would you like to add?");
+    userInput = Console.ReadLine();
+
+    if (userInput == "")
     {
-      Console.Write("Which TODO task would you like to remove? ");
-      SeeAllTodos();
+      System.Console.WriteLine("Please type literally anything, idiot...");
+    }
+    else if (toDoList.Contains(userInput))
+    {
+      System.Console.WriteLine("You already have that as a task jackass...");
+    }
+  } while (userInput == "" || toDoList.Contains(userInput));
 
-      input = Console.ReadLine();
-
-      // keep looping while input is NOT a valid index
-    } while (!int.TryParse(input, out indexInput) ||
-           indexInput < 1 ||
-           indexInput > toDoList.Count);
-
-    toDoList.RemoveAt(indexInput - 1);
-    return toDoList;
-  }
+  toDoList.Add(userInput);
+  System.Console.WriteLine("TODO has been successfully added!😃");
 }
-
-bool Exit()
-{
-  System.Console.WriteLine("Thank you! See you later alligator!");
-  return shallExit = true;
-}
-
 
 Console.ReadKey();
